3759f269d00c0e985a82446cbba17de0e0da2c43
Kiriakos alexiou <kiriakosal2004@yahoo.gr>
2023-11-26 21:20:22 +0200

    updated django forms

--- Marine_fuel_cons/fuel_cons_pred/views.py
+++ Marine_fuel_cons/fuel_cons_pred/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html', {})
+
+def prediction(request):
+    if request.method == 'POST':
+        duration = request.POST.get('duration')
+        speed = request.POST.get('speed')
+        aft_draft = request.POST.get('aft_draft')
+        fore_draft = request.POST.get('fore_draft')
+        heading = request.POST.get('heading')     
+    return render(request, 'prediction.html', {'speed':speed})
